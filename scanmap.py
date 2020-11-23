d441817c7c802cf3cc6bf306c5dbe4aa5d54f2cd
Pedro Castro <pedroddcastro21@gmail.comgit config --global user.email pedroddcastro21@gmail.comgit config --global user.email pedroddcastro21@gmail.com>
2020-11-23 00:17:52 -0300

    add figlet/loading

--- scanmap.py
+++ scanmap.py
@@ -1,13 +1,30 @@
 import os
 import sys
 import nmap3
+import time
 nm = nmap3.Nmap()
 
 ping = os.system("ping -c 1 " + sys.argv[1]) #server up or down
 
 results = nm.nmap_version_detection(sys.argv[1]) #nmap -sV dict
 
-print ('####################\n')
+os.system("clear")
+os.system("figlet ScaNmap")
+
+results = nm.nmap_version_detection(sys.argv[1]) #nmap -sV dict
+
+print ("[                    ] 0% ")
+time.sleep(5)
+print ("[=====               ] 25%")
+time.sleep(5)
+print ("[==========          ] 50%")
+time.sleep(5)
+print ("[===============     ] 75%")
+time.sleep(5)
+print ("[====================] 100%")
+time.sleep(3)
+
+print ('\n####################\n')
 print ('[+]HOST: ' + sys.argv[1])
 
 #server up or down
@@ -32,5 +49,6 @@ for dict in results:
         print ('[+]SERVICE:', dict['service']['name'])
 
 print('MAIN PORTS: 20, 21, 22, 23, 25(587), 53, 80, 443, 8080, 43')
+    
     
     
